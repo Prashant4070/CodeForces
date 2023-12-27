ec48e96a761536bc0972eba16f2d43c6265a615c
Prashant Kumar Singh <55409264+Prashant4070@users.noreply.github.com>
2023-12-27 12:31:09 +0530

    Solution to problem 59A

---
+++ 59A.py
@@ -0,0 +1,12 @@
+word= input()
+lower=0
+upper=0
+for i in word:
+    if i.lower()==i:
+        lower+=1
+    else:
+        upper+=1
+if lower>=upper:
+    print(word.lower())
+else:
+    print(word.upper())
